f3419633fd49220f270a8e595fa2a4535caac800
Jaro <smekja@seznam.cz>
2020-06-04 17:12:44 +0200

    Update

--- app.js
+++ app.js
@@ -50,4 +50,5 @@ app.get("/admin", (req,resp) => {
 app.get("/login", (req, resp) => {
     resp.render("login");
 })
-app.listen(3000, () => console.log("Listening 3000..."));
+let port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening ${port}...`));
